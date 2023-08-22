1903382241ea53e9114031366e644346a755fe01
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-08-22 20:20:20 -0300

    Finishing the Exercise

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const nameElement = document.querySelector(".name");
-    const usernameElement = document.querySelector(".username");
-    const avatarElement = document.querySelector(".avatar");
-    const reposElement = document.querySelector(".repos");
-    const followersElement = document.querySelector(".followers");
-    const followingElement = document.querySelector(".following");
-    const linkElement = document.querySelector(".link");
+    const nameElement = document.querySelector(".profile-name");
+    const usernameElement = document.querySelector(".profile-username");
+    const avatarElement = document.querySelector(".profile-avatar");
+    const reposElement = document.querySelector(".numbers-item-repos");
+    const followersElement = document.querySelector(".numbers-item-followers");
+    const followingElement = document.querySelector(".numbers-item-following");
+    const linkElement = document.querySelector(".profile-link");
 
     fetch("https://api.github.com/users/Claudenir-Nojosa")
         .then(function(res) {
@@ -16,8 +16,8 @@ document.addEventListener("DOMContentLoaded", function() {
             usernameElement.innerText = json.login;
             avatarElement.src = json.avatar_url;
             reposElement.innerText = json.public_repos;
-            followersElement.innerText = json.followers_url;
-            followingElement.innerText = json.following_url;
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
             linkElement.href = json.html_url;
         })
 })
